d5f07e3771c8310e5c1d969113ff8fdb003603ab
lennardevertz <levertz@idriss.xyz>
2024-02-29 00:11:19 +0100

    happy path bridge eth base -> op

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,10 +1,10 @@
 console.log("Hello Cross-chain Gitcoin donations!");
-// Ethers V6
-import {ethers} from "ethers";
+// Ethers V5
+import {BigNumberish, ethers} from "ethers";
 
 import spoolABI from "../../contracts/abi/spokePool.json";
-import { ISpokePool } from "./spool";
-
+import {ISpokePool} from "./static/spool";
+console.log(ethers.version);
 declare global {
     interface Window {
         ethereum: any;
@@ -13,13 +13,13 @@ declare global {
 
 type SpoolContract = ethers.Contract & ISpokePool;
 
-
-let provider: ethers.BrowserProvider;
+let provider: ethers.providers.Web3Provider;
 let signer: ethers.Signer;
 let contractOrigin: SpoolContract;
+let availableRoutes;
 
 function generateMessage(userAddress: string) {
-    const abiCoder = ethers.AbiCoder.defaultAbiCoder();
+    const abiCoder = ethers.utils.defaultAbiCoder;
     return abiCoder.encode(["address"], [userAddress]);
 }
 
@@ -73,10 +73,14 @@ const CONTRACTS = {
             blockNumber: 4822423,
         },
     },
-    "420": {
+    "10": {
         SpokePool: {
-            address: "0xeF684C38F94F48775959ECf2012D7E864ffb9dd4",
-            blockNumber: 17025501,
+            address: "0x6f26Bf09B1C792e3228e5467807a900A503c0281",
+            blockNumber: 93903076,
+        },
+        SpokePoolVerifier: {
+            address: "0x269727F088F16E1Aea52Cf5a97B1CD41DAA3f02D",
+            blockNumber: 110419958,
         },
     },
 };
@@ -106,9 +110,6 @@ async function callAcrossAPI(
     }
 }
 
-const originChainId = 11155111;
-const destinationChainId = 84532;
-
 document
     .getElementById("connectWalletButton")
     ?.addEventListener("click", async () => {
@@ -118,7 +119,7 @@ document
                 await window.ethereum.request({method: "eth_requestAccounts"});
 
                 // Create a Web3 provider from the window.ethereum object
-                provider = new ethers.BrowserProvider(window.ethereum);
+                provider = new ethers.providers.Web3Provider(window.ethereum);
 
                 // You now have access to the user's wallet
                 signer = await provider.getSigner();
@@ -140,11 +141,14 @@ document
                 contractOrigin = new ethers.Contract(
                     CONTRACTS["8453"].SpokePool.address,
                     spoolABI,
-                    provider
+                    signer
                 ) as SpoolContract;
-                console.log("Contract set up on Base: ", await contractOrigin.getAddress())
-                const chainId = await contractOrigin.chainId()
-                console.log("chainId is ", chainId)
+                console.log(
+                    "Contract set up on Base: ",
+                    await contractOrigin.address
+                );
+                const chainId = await contractOrigin.chainId();
+                console.log("chainId is ", chainId);
             } catch (error) {
                 console.error("Error creating contract:", error);
             }
@@ -155,4 +159,170 @@ document
         }
     });
 
+document.getElementById("callBridge")?.addEventListener("click", async () => {
+    if (typeof provider !== "undefined") {
+        try {
+            const originChainId = 8453 as number;
+            const destinationChainId = 10 as number;
+            const token = "0x4200000000000000000000000000000000000006";
+            const amount = ethers.BigNumber.from("4000000000000000");
 
+            availableRoutes = await callAcrossAPI(endpoints.routes, {});
+
+            const suggested_fees = await callAcrossAPI(endpoints.fee, {
+                originChainId: originChainId,
+                destinationChainId: destinationChainId,
+                token: token,
+                amount: amount.toString(),
+            });
+            console.log(suggested_fees);
+            const totalFee = suggested_fees.totalRelayFee.total;
+            console.log("Total fee ", totalFee);
+            console.log("When sending ", amount.toString(), " from base to op");
+            console.log(
+                "Sending a total of ",
+                ethers.BigNumber.from(totalFee).add(amount).toString(),
+                " from base to op"
+            );
+            const timestamp = suggested_fees.timestamp as number;
+            console.log("Timestamp", timestamp);
+            const limits = await callAcrossAPI(endpoints.limits, {
+                originChainId: originChainId,
+                destinationChainId: destinationChainId,
+                token: token,
+            });
+            console.log("limits: ", limits);
+            const maxDepositInstant = limits.maxDepositInstant;
+            const maxDepositShortDelay = limits.maxDepositShortDelay;
+            const maxDeposit = limits.maxDeposit;
+            console.log(
+                "maxDepositInstant",
+                maxDepositInstant,
+                amount < ethers.BigNumber.from(maxDepositInstant)
+            );
+            console.log(
+                "maxDepositShortDelay",
+                maxDepositShortDelay,
+                amount < ethers.BigNumber.from(maxDepositShortDelay)
+            );
+            console.log(
+                "maxDeposit",
+                maxDeposit,
+                amount < ethers.BigNumber.from(maxDeposit)
+            );
+            await depositToSpokePool(
+                await signer.getAddress(),
+                token,
+                amount,
+                {total: totalFee},
+                destinationChainId,
+                timestamp
+            );
+        } catch (error) {
+            console.error("Error creating contract:", error);
+        }
+    } else {
+        console.log("No provider detected. Please connect your wallet first.");
+    }
+});
+
+async function depositToSpokePool(
+    userAddress: string,
+    assetAddress: string,
+    amount: ethers.BigNumber,
+    totalRelayFee: {total: string},
+    destinationChainId: number,
+    timestamp: number
+) {
+    try {
+        const outputToken = "0x0000000000000000000000000000000000000000";
+        const exclusiveRelayer = "0x0000000000000000000000000000000000000000";
+        const fillDeadline = Math.round(Date.now() / 1000) + 21600; // 6 hours from now
+        const exclusivityDeadline = 0;
+        const message = "0x"; // for normal bridge
+
+        const outputAmount = ethers.BigNumber.from(amount).sub(
+            ethers.BigNumber.from(totalRelayFee.total)
+        );
+        console.log(outputAmount);
+
+        let params = [
+            userAddress,
+            userAddress,
+            assetAddress,
+            outputToken,
+            amount,
+            outputAmount,
+            destinationChainId,
+            exclusiveRelayer,
+            timestamp,
+            fillDeadline,
+            exclusivityDeadline,
+            message,
+        ];
+        console.log(params);
+
+        const args = [userAddress,
+            userAddress,
+            assetAddress,
+            outputToken,
+            amount,
+            outputAmount,
+            destinationChainId,
+            exclusiveRelayer,
+            timestamp,
+            fillDeadline,
+            exclusivityDeadline,
+            message]
+
+        const preparedTx = await contractOrigin.populateTransaction["depositV3"](...args)
+
+        console.log(preparedTx)
+        console.log({...preparedTx})
+        console.log(...args)
+
+        const sendOptions = {
+            from: await signer.getAddress(),
+            value: amount.toString()
+        };
+
+        const result = await signer.sendTransaction({
+            ...preparedTx,
+            ...sendOptions,
+            to: contractOrigin.address,
+          });
+        const minedResult = await result.wait();
+
+        console.log(minedResult)
+          
+
+        // const tx = contractOrigin.depositV3(
+        //     userAddress,
+        //     userAddress,
+        //     assetAddress,
+        //     outputToken,
+        //     amount,
+        //     outputAmount,
+        //     destinationChainId,
+        //     exclusiveRelayer,
+        //     timestamp,
+        //     fillDeadline,
+        //     exclusivityDeadline,
+        //     message
+        // );
+        // (await tx).value = amount;
+
+        // tx.
+        // const approveTxSigned = await signer.signTransaction(tx)
+
+        // const submittedTx = await provider.sendTransaction(approveTxSigned);
+        // const approveReceipt = await submittedTx.wait();
+        // if (approveReceipt.status === 0)
+        //     throw new Error("Approve transaction failed");
+
+        console.log("Deposit successful");
+    } catch (error) {
+        console.error("Error in deposit:", error);
+        throw error;
+    }
+}
