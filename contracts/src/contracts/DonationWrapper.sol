dd49f870772f504456582e4f54f4e6b19f66b855
lennardevertz <levertz@idriss.xyz>
2024-03-20 16:27:25 +0100

    - add dummy allo logic

--- contracts/src/contracts/DonationWrapper.sol
+++ contracts/src/contracts/DonationWrapper.sol
@@ -5,6 +5,7 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 import {PublicGoodAttester} from "./libs/Attestation.sol";
 import {IDonations} from "./interfaces/IDonations.sol";
+import {IAllo} from "./interfaces/IAllo.sol";
 import {V3SpokePoolInterface} from "./interfaces/ISpokePool.sol";
 import {WETH9Interface} from "./interfaces/IWETH.sol";
 
@@ -13,9 +14,11 @@ contract DonationWrapper is Ownable, PublicGoodAttester {
 
     address public SPOKE_POOL;
     address public DONATION_ADDRESS;
+    address public ALLO;
     address public WETH_ADDRESS;
     V3SpokePoolInterface spokePool;
     IDonations donationsContract;
+    IAllo alloContract;
     WETH9Interface wethContract;
 
     struct DepositParams {
@@ -31,18 +34,37 @@ contract DonationWrapper is Ownable, PublicGoodAttester {
         uint32 exclusivityDeadline;
     }
 
+    struct TokenPermissions {
+        address token;
+        uint256 amount;
+    }
+
+    struct PermitTransferFrom{
+        TokenPermissions permitted;
+        uint256 nonce;
+        uint256 deadline;
+    }
+
+    struct Permit2Data {
+        PermitTransferFrom permit;
+        bytes signature;
+    }
+
     constructor(
         address _eas,
         bytes32 _easSchema,
         address _acrossSpokePool,
         address _donationsContractAddress,
+        address _allo,
         address _wethAddress
     ) PublicGoodAttester(_eas, _easSchema) {
         SPOKE_POOL = _acrossSpokePool;
         DONATION_ADDRESS = _donationsContractAddress;
         WETH_ADDRESS = _wethAddress;
+        ALLO_ADDRESS = _allo;
         spokePool = V3SpokePoolInterface(SPOKE_POOL);
         donationsContract = IDonations(DONATION_ADDRESS);
+        alloContract = IALLO(ALLO_ADDRESS);
         wethContract = WETH9Interface(WETH_ADDRESS);
     }
 
@@ -63,8 +85,9 @@ contract DonationWrapper is Ownable, PublicGoodAttester {
             address grantee,
             uint256 granteeId,
             address round,
-            uint256 applicationIndex
-        ) = abi.decode(message, (address, address, uint256, address, uint256));
+            uint256 applicationIndex,
+            uint256 poolId
+        ) = abi.decode(message, (address, address, uint256, address, uint256, uint256));
 
         _attestDonor(donor, grantee, granteeId, round, tokenSent, amount);
 
@@ -76,9 +99,28 @@ contract DonationWrapper is Ownable, PublicGoodAttester {
             applicationIndex
         );
 
-        donationsContract.vote{value: amount}(encodedVote, round, tokenSent);
+// figure out ISignatureTransfer and deadline -> not needed as `token: NATIVE`? Same with `nonce`?
+        Permit2Data memory permit2Data =
+        Permit2Data({
+            permit: PermitTransferFrom({
+                permitted: TokenPermissions({token: NATIVE, amount: amount}),
+                nonce: 0,
+                deadline: 0
+            }),
+            signature: ""
+        });
 
+        _vote(poolId, recipientId, permit2Data);
     }
+
+// DonationVotingMerkleDistributionBaseStrategy.PermitType.None == 0 ?
+// granteeId == recipientId address?
+    function _vote(uint256 poolId, address recipientId, Permit2Data permit2Data) internal {
+        alloContract.allocate{value: permit2Data.permit.permitted.amount}(
+            poolId, abi.encode(granteeId, 0, permit2Data)
+        );
+    }
+
 
     function callDepositV3(
         DepositParams memory params,
