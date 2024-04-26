4322307b85cb99d52a71abffc17e0619171f6a59
lennardevertz <levertz@idriss.xyz>
2024-04-26 17:36:08 +0200

    - use ECDSA.recover instead of ecrecover

--- contracts/src/contracts/DonationWrapper.sol
+++ contracts/src/contracts/DonationWrapper.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.19;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 import {PublicGoodAttester} from "./libs/Attestation.sol";
 
@@ -271,7 +272,7 @@ contract DonationWrapper is
     }
 
     /**
-     * @notice Verifies a signed message using ecrecover.
+     * @notice Verifies a signed message using ECDSA.recover.
      * @param _signer The expected signer's address.
      * @param _message The original message that was signed.
      * @param _signature The signature to verify.
@@ -285,29 +286,9 @@ contract DonationWrapper is
         bytes32 messageHash = getMessageHash(_message);
         bytes32 ethSignedMessageHash = getEthSignedMessageHash(messageHash);
 
-        return recoverSigner(ethSignedMessageHash, _signature) == _signer;
+        return ECDSA.recover(ethSignedMessageHash, _signature) == _signer;
     }
 
-    function recoverSigner(
-        bytes32 _ethSignedMessageHash,
-        bytes memory _signature
-    ) public pure returns (address) {
-        (bytes32 r, bytes32 s, uint8 v) = splitSignature(_signature);
-
-        return ecrecover(_ethSignedMessageHash, v, r, s);
-    }
-
-    function splitSignature(
-        bytes memory sig
-    ) public pure returns (bytes32 r, bytes32 s, uint8 v) {
-        require(sig.length == 65, "invalid signature length");
-
-        assembly {
-            r := mload(add(sig, 32))
-            s := mload(add(sig, 64))
-            v := byte(0, mload(add(sig, 96)))
-        }
-    }
 
     /**
      * @notice Withdraw accidentally sent native currency
