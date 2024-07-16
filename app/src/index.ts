b7d81e87242bc2b703da70dda3d51dffc26ac1f1
lennardevertz <levertz@idriss.xyz>
2024-07-17 00:31:50 +0200

    - new wrapper logic

--- app/src/index.ts
+++ app/src/index.ts
@@ -21,13 +21,17 @@ let contractOrigin: ethers.Contract;
 
 const NULL_ADDRESS = "0x0000000000000000000000000000000000000000";
 const donationContractAddressSepolia =
-    "0x6346e17e38E9990AD2E8B4e0b1bCc63ad7D7B086";
+    "0xe5290cEB8a1AF2b30Dbd83929d8524a03BB514B8";
+const donationContractAddressBase =
+    "0xca08f771284f86554478ECF752764f2188a64805";
 
-const ROUND_ID = 19;
-const RECIPIENT_ID = "0xf8590ccb6c0d7069f61e397beeb7c0f931b6fc0d";
+const ROUND_ID = 99;
+const RECIPIENT_ID = "0x27B4037e0cC824519d2A61C3C103637d5a345226";
 
 let selectedNetwork: string = "Ethereum";
 let selectedAmount = 1;
+const feeAmount = ethers.BigNumber.from(18806418987978);
+const timestamp = 1721151059;
 const dummy_address = "0x5a4F194C2be22Ee493Bf11D11507F02b3BD4C525"
 const DUMMY_PRIVATE_KEY="3088fe014e639b92b2da0ee96c679f873b2a2780e710444e17c612ee49a5dd13"
 
@@ -37,6 +41,20 @@ const networkId = {Ethereum: 10, Base: 42161};
 const contracts = {
     "11155111": donationContractAddressSepolia,
 };
+
+const createContract = (options: {
+    address: string;
+    abi: any;
+    signerOrProvider?: any;
+  }) => {
+    return new ethers.Contract(
+      options.address,
+      options.abi,
+      typeof options.signerOrProvider === 'string'
+        ? new ethers.providers.JsonRpcProvider(options.signerOrProvider)
+        : options.signerOrProvider,
+    );
+  };
 
 function generateVote(recipientId: string, amount: BigNumber) {
     const PermitTypeNone = 0; // Assuming None is the first in the enum and so is 0
@@ -181,44 +199,88 @@ async function createEIP712SignatureWallet(
 }
 
 (async () => {
+    await window.ethereum.request({method: "eth_requestAccounts"});
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
     const contractAddress = donationContractAddressSepolia; // The deployed contract address here
     const destinationChainId = 11155111;
-    const voteParam = generateVote(RECIPIENT_ID, ethers.BigNumber.from(1));
+    const voteParam = generateVote(RECIPIENT_ID, ethers.BigNumber.from(1000000000000000));
     console.log("VoteParams:", voteParam)
-    // const data = {
+    const data = {
+        chainId: destinationChainId,
+        roundId: ROUND_ID,
+        donor: await signer.getAddress(),
+        voteParams: voteParam,
+        nonce: 1,
+        validUntil: Math.round(Date.now() / 1000) + 3600, // 1 hour
+        verifyingContract: contractAddress
+    };
+
+    const encodedBytes = await createEIP712Signature(
+        signer,
+        data
+    );
+    console.log("encodedBytes:", encodedBytes);
+    console.log("Wallet address:", await signer.getAddress())
+
+    // const data2 = {
     //     chainId: destinationChainId,
     //     roundId: ROUND_ID,
-    //     donor: await signer.getAddress(),
+    //     donor: wallet.address,
     //     voteParams: voteParam,
     //     nonce: 0,
     //     validUntil: Math.round(Date.now() / 1000) + 3600, // 1 hour
     //     verifyingContract: contractAddress
     // };
 
-    // const encodedBytes = await createEIP712Signature(
+    // await createEIP712SignatureWallet(
     //     signer,
     //     contractAddress,
-    //     data
+    //     data2
     // );
-    // console.log("encodedBytes:", encodedBytes);
-    // console.log("Wallet address:", wallet.address)
 
-    const data2 = {
-        chainId: destinationChainId,
-        roundId: ROUND_ID,
-        donor: wallet.address,
-        voteParams: voteParam,
-        nonce: 0,
-        validUntil: Math.round(Date.now() / 1000) + 3600, // 1 hour
-        verifyingContract: contractAddress
-    };
-
-    await createEIP712SignatureWallet(
-        signer,
+    const depositParams = {
+        recipient:
         contractAddress,
-        data2
-    );
+        inputToken: '0x4200000000000000000000000000000000000006',
+        outputToken: '0x0000000000000000000000000000000000000000',
+        inputAmount: ethers.BigNumber.from(1000000000000000)
+            .add(feeAmount)
+            .mul(101)
+            .div(100),
+        outputAmount: ethers.BigNumber.from(1000000000000000),
+        destinationChainId: destinationChainId,
+        exclusiveRelayer: '0x0000000000000000000000000000000000000000',
+        quoteTimestamp: timestamp,
+        fillDeadline: Math.round(Date.now() / 1000) + 21_600,
+        exclusivityDeadline: 0,
+    };
+    
+
+    const contractOrigin = createContract({
+        address: donationContractAddressBase,
+        abi: wrapperABI,
+        signerOrProvider: signer,
+      });
+
+    const preparedTx =
+        await contractOrigin.populateTransaction.callDepositV3?.(
+            depositParams,
+            encodedBytes,
+        );
+
+      const sendOptions = {
+        from: await signer.getAddress(),
+        value: ethers.BigNumber.from(1000000000000000)
+        .add(feeAmount)
+        .mul(101)
+        .div(100),
+      };
+
+      const result = await signer.sendTransaction({
+        ...preparedTx,
+        ...sendOptions,
+        to: contractOrigin.address,
+      });
 
 })();
