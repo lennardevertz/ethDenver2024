bff3bf63c9e742da3ce83c69fc425aec4eb8b837
lennardevertz <levertz@idriss.xyz>
2024-07-22 23:08:47 +0200

    - adjust deployment params

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -55,11 +55,16 @@ const config: HardhatUserConfig = {
           url: 'https://rpc.linea.build',
           accounts: [process.env.PRIVATE_KEY!],
         },
+        zkSyncMainnet: {
+          chainId: 324,
+          url: 'hhttps://mainnet.era.zksync.io',
+          accounts: [process.env.PRIVATE_KEY!],
+        },
         "base-sepolia": {
           chainId: 84532,
           url: 'https://sepolia.base.org',
           accounts: [process.env.PRIVATE_KEY!],
-        },
+        },        
         hardhat_node: {
             chainId: 1337,
             url: "http://127.0.0.1:8545",
@@ -76,7 +81,8 @@ const config: HardhatUserConfig = {
           optimisticEthereum: process.env.OPSCAN_KEY!,
           arbitrum: process.env.ARBSCAN_KEY!,
           arbitrumOne: process.env.ARBSCAN_KEY!,
-          linea_mainnet: process.env.LINEASCAN_KEY!
+          linea_mainnet: process.env.LINEASCAN_KEY!,
+          zkSyncMainnet: process.env.ETHERSCAN_KEY!
         },
         customChains: [
           {
@@ -91,10 +97,50 @@ const config: HardhatUserConfig = {
             network: "base",
             chainId: 8453,
             urls: {
-             apiURL: "https://api.basescan.org/api",
-             browserURL: "https://basescan.org"
+              apiURL: "https://base.blockscout.com/api",
+              browserURL: "https://base.blockscout.com/",
             }
-          }
+          },
+          {
+            network: "optimism",
+            chainId: 10,
+            urls: {
+              apiURL: "https://optimism.blockscout.com/api",
+              browserURL: "https://optimism.blockscout.com/",
+            }
+          },
+          {
+            network: "arbitrum",
+            chainId: 42161,
+            urls: {
+              apiURL: "https://arbitrum.blockscout.com/api",
+              browserURL: "https://arbitrum.blockscout.com/",
+            }
+          },
+          {
+            network: "mainnet",
+            chainId: 1,
+            urls: {
+              apiURL: "https://eth.blockscout.com/api",
+              browserURL: "https://eth.blockscout.com/",
+            }
+          },
+          {
+            network: "linea_mainnet",
+            chainId: 59144,
+            urls: {
+              apiURL: "https://api.lineascan.build/api",
+              browserURL: "https://lineascan.build/"
+            }
+          },
+          {
+            network: "zkSyncMainnet",
+            chainId: 42161,
+            urls: {
+              apiURL: "https://zksync.blockscout.com/api",
+              browserURL: "https://zksync.blockscout.com/",
+            }
+          },
         ]
       }
 };
